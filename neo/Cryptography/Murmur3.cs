d2c8f37897fb789d05e373428c9ae559ce786568
DijoinKlink <32598239+DijoinKlink@users.noreply.github.com>
2017-10-11 11:26:22 -0600

    Update Murmur3.cs

--- neo/Cryptography/Murmur3.cs
+++ neo/Cryptography/Murmur3.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
-namespace Neo.Cryptography
+namespace LightningX.Cryptography
 {
     public class Murmur3 : HashAlgorithm
     {
